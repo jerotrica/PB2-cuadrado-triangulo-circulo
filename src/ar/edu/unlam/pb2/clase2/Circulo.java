a4b355f47cab5e23d3d1240e7abe8003b2ab09c6
Jeronimo Romero Tricarico <jerotrica@gmail.com>
2016-09-04 16:22:58 -0300

    Terminada la identacion, Todo testeado

--- src/ar/edu/unlam/pb2/clase2/Circulo.java
+++ src/ar/edu/unlam/pb2/clase2/Circulo.java
@@ -3,21 +3,21 @@ package ar.edu.unlam.pb2.clase2;
 public class Circulo {
 	private Float radio, perimetroCirculo,areaCirculo;	
 	
-
-public void calcularPerimetro (Float radio){
-	perimetroCirculo =(float) (radio *2*3.14);
-}
-
-public void calcularArea (Float radio){
-	areaCirculo =(float) Math.pow(radio*3.14, 2);
-}
-
-public float visualizarPerimetro(){
-	return this.perimetroCirculo;
-}
-
-public float visualizarArea(){
-	return this.areaCirculo;
-}
+	
+	public void calcularPerimetro (Float radio){
+		perimetroCirculo =(float) (radio *2*3.14);
+	}
+	
+	public void calcularArea (Float radio){
+		areaCirculo =(float) Math.pow(radio*3.14, 2);
+	}
+	
+	public float visualizarPerimetro(){
+		return this.perimetroCirculo;
+	}
+	
+	public float visualizarArea(){
+		return this.areaCirculo;
+	}
 
 }
--- src/ar/edu/unlam/pb2/clase2/Cuadrado.java
+++ src/ar/edu/unlam/pb2/clase2/Cuadrado.java
@@ -4,19 +4,19 @@ public class Cuadrado {
 	private Float ladoCuadrado, perimetroCuadrado,areaCuadrado;	
 	
 
-public void calcularPerimetro (Float ladoCuadrado){
-	perimetroCuadrado =ladoCuadrado *4;
-}
-
-public void calcularArea (Float ladoCuadrado){
-	areaCuadrado =ladoCuadrado *ladoCuadrado;
-}
-
-public float visualizarPerimetro(){
-	return this.perimetroCuadrado;
-}
-
-public float visualizarArea(){
-	return this.areaCuadrado;
-}
-}
+	public void calcularPerimetro (Float ladoCuadrado){
+		perimetroCuadrado =ladoCuadrado *4;
+	}
+	
+	public void calcularArea (Float ladoCuadrado){
+		areaCuadrado =ladoCuadrado *ladoCuadrado;
+	}
+	
+	public float visualizarPerimetro(){
+		return this.perimetroCuadrado;
+	}
+	
+	public float visualizarArea(){
+		return this.areaCuadrado;
+	}
+	}
